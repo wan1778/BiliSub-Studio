2264c0681edf0f885fbc68ae7c1646eafbac3570
wan1778 <mantran462003@gmail.com>
2026-08-23 22:58:48 +0700

    Editor: wire image feature WinUI namespaces

---
+++ csharp/src/BiliSubStudio.App/EditorImageGlobalUsings.cs
@@ -0,0 +1,4 @@
+global using BiliSubStudio.Core.Jobs;
+global using Microsoft.UI.Xaml.Automation;
+global using Microsoft.UI.Xaml.Automation.Peers;
+global using Microsoft.UI.Xaml.Controls.Primitives;
