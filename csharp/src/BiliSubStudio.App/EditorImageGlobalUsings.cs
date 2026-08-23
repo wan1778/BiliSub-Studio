b9bfc7b7601ed5f2534dd15b0381291366f75854
wan1778 <mantran462003@gmail.com>
2026-08-23 23:07:44 +0700

    fix: disambiguate editor image file paths from XAML Path

--- csharp/src/BiliSubStudio.App/EditorImageGlobalUsings.cs
+++ csharp/src/BiliSubStudio.App/EditorImageGlobalUsings.cs
@@ -2,3 +2,4 @@ global using BiliSubStudio.Core.Jobs;
 global using Microsoft.UI.Xaml.Automation;
 global using Microsoft.UI.Xaml.Automation.Peers;
 global using Microsoft.UI.Xaml.Controls.Primitives;
+global using Path = System.IO.Path;
